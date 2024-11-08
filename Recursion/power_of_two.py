65cfed4f362f7943202d402626897aad7ab075e4
Aakanksha Padmanabhan <58661363+aakanksha-1399@users.noreply.github.com>
2024-11-07 19:47:07 -0500

    Create power_of_two.py

---
+++ Recursion/power_of_two.py
@@ -0,0 +1,12 @@
+# check if the number is divisible by 2 if yes till the end, then it is power of two, else it is not
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        print(n//2)
+        if n == 1:
+            return True
+        if n % 2 == 1 or n == 0:
+            return False
+
+        return self.isPowerOfTwo(n//2)
+        
